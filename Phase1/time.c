5821be3fc8fae965210d94d6a7b41451a5771a87
parham <parham.hessabi@gmail.com>
2017-11-07 19:22:29 +0330

    comments added to time.c

--- Phase1/time.c
+++ Phase1/time.c
@@ -3,8 +3,8 @@
 
 int main()
 {
-	int t = time(0);
-	printf ("%s", ctime(&t));
+	int t = time(0); //syscall for getting the system current time 
+	printf ("%s", ctime(&t)); //converting format to standard
 	return 0;
 }
 
